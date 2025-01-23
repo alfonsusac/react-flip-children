36f519b2a8d61aee0572f2e70c18b55c4ef107a8
Alfonsus Ardani <alfonsusac@gmail.com>
2025-01-23 22:31:27 +0700

    build commit

--- lib/AnimateChildren/dist/index.d.ts
+++ lib/AnimateChildren/dist/index.d.ts
@@ -1,6 +1,8 @@
 import { type ReactNode } from "react";
 export declare function AnimateChildren({ children, ...props }: {
     children?: ReactNode;
+    easing?: KeyframeAnimationOptions["easing"];
+    duration?: number;
     normalizeKeys?: boolean;
     delayDeletion?: number;
     useAbsolutePositionOnDeletedElements?: boolean;
